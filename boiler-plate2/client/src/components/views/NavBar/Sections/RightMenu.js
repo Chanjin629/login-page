fc6ca24c334da51118c703507a70e2df3ac58b3e
Kim, Chanjin <ihercwdxz@gmail.com>
2023-02-27 23:58:22 +0900

    navbar

--- boiler-plate2/client/src/components/views/NavBar/Sections/RightMenu.js
+++ boiler-plate2/client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import { Menu } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+import axios from 'axios';
+import { useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
 
-function LeftMenu(props) {
-  return (
-    <Menu mode={props.mode}>
-    <Menu.Item key="mail">
-      <a href="/">Home</a>
-    </Menu.Item>
-    <SubMenu title={<span>Blogs</span>}>
-      <MenuItemGroup title="Item 1">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
-      </MenuItemGroup>
-      <MenuItemGroup title="Item 2">
-        <Menu.Item key="setting:3">Option 3</Menu.Item>
-        <Menu.Item key="setting:4">Option 4</Menu.Item>
-      </MenuItemGroup>
-    </SubMenu>
-  </Menu>
-  )
+function RightMenu(props) {
+  const user = useSelector(state => state.user);
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    axios.get(`/api/users/logout`).then(response => {
+      if (response.status === 200) {
+        navigate("/login");
+      } else {
+        alert('Log Out Failed')
+      }
+    });
+  };
+
+  if (user.userData && !user.userData.isAuth) {
+    return (
+      <Menu mode={props.mode}>
+        <Menu.Item key="mail">
+          <a href="/login">Signin</a>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <a href="/register">Signup</a>
+        </Menu.Item>
+      </Menu>
+    )
+  } else {
+    return (
+      <Menu mode={props.mode}>
+        <Menu.Item key="logout">
+          <a onClick={logoutHandler}>Logout</a>
+        </Menu.Item>
+      </Menu>
+    )
+  }
 }
 
-export default LeftMenu
+export default RightMenu;
